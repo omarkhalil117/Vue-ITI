c8319b5537dc090be05081382f68099893404a51
omarkhalil117 <omarkhalil117@gmail.com>
2024-04-12 10:27:30 +0200

    Lab 2 Bookstore & wishlist

---
+++ Lab [2]/src/components/BookCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card mb-4 h-100 mt-3 shadow-sm">
+    <img
+      :src="book.image"
+      class="card-img-top"
+      alt="Book Cover"
+      style="width: 350px; height: 250px; object-fit: fill"
+    />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title text-center">{{ book.name }}</h5>
+      <div class="d-flex justify-content-between">
+        <div>
+          <p class="card-text">Author: {{ book.author }}</p>
+          <p class="card-text">
+            Category: {{ book.category }}
+          </p>
+        </div>
+        <div>
+          <div :class="{'more': book.numberOfPages>200 , 'less': book.numberOfPages<300}">
+            <p>{{ book.numberOfPages }} page</p>
+          </div>
+          <p class="card-text">
+            Price: {{Intl.NumberFormat("ar-SA", {style: "currency",currency: "SAR",}).format(book.price)}}
+          </p>
+        </div>
+      </div>
+      <button @click.prevent class="btn btn-primary mt-3 align-self-end  btn-sm" @click="addToWishlist(book)">Add to Wish List</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["book", "addToWishlist", "formatPrice"],
+  mounted() {
+    console.log(this.book.image);
+  },
+};
+</script>
---
+++ Lab [2]/src/components/BookList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   
+    <!-- Names List -->
+    <div class="container text-center">
+    <ul>
+      <li class="list-group-item text-decoration-none " v-for="book in books" :key="book.id" >
+        {{ book.name }}
+      </li>
+    </ul>
+    </div>
+
+
+    <h1 class="text-center">Book Store</h1>
+    <div class="row justify-content-center">
+      <div class="col-4 mb-4" v-for="book in books" :key="book.id">
+        <BookCard
+          :book="book"
+          :addToWishlist="addToWishlist"
+          :formatPrice="formatPrice"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BookCard from "./BookCard.vue";
+
+export default {
+  components: {
+    BookCard,
+  },
+  props: ["books", "addToWishlist", "formatPrice"],
+};
+</script>
